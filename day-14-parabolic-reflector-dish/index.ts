eff606de5d5952a6480df75099a2bb56cfe94c44
galnegus <tobiashindersson@hotmail.com>
2023-12-14 22:39:26 +0100

    day 14 refactored

--- day-14-parabolic-reflector-dish/index.ts
+++ day-14-parabolic-reflector-dish/index.ts
@@ -6,84 +6,78 @@ const rawInput: string = require("fs").readFileSync(
   "utf-8"
 );
 
-const dish = rawInput
+type Dish = Array<Array<string>>;
+const dishOne: Dish = rawInput
   .split(/\r?\n/)
   .filter(Boolean)
   .map((line) => line.split(""));
+const dishTwo = structuredClone(dishOne);
 
-function tiltNorth(): void {
+function moveO(dish: Dish, fromRow: number, fromCol: number, toRow: number, toCol: number): void {
+  dish[fromRow][fromCol] = ".";
+  dish[toRow][toCol] = "O";
+}
+
+function tiltNorth(dish: Dish): void {
   for (let row = 0; row < dish.length; ++row) {
     for (let col = 0; col < dish[0].length; ++col) {
       if (dish[row][col] !== "O") continue;
-      let finalRow = row;
-      for (let tryRow = row - 1; tryRow >= 0; --tryRow) {
-        if (dish[tryRow][col] === ".") finalRow = tryRow;
-        else break;
-      }
-      if (finalRow !== row) {
-        dish[row][col] = ".";
-        dish[finalRow][col] = "O";
-      }
+      let move;
+      for (move = row - 1; move >= 0; --move) if (dish[move][col] !== ".") break;
+      if (move + 1 !== row) moveO(dish, row, col, move + 1, col);
     }
   }
 }
-function tiltSouth(): void {
+function tiltSouth(dish: Dish): void {
   for (let row = dish.length - 1; row >= 0; --row) {
     for (let col = 0; col < dish[0].length; ++col) {
       if (dish[row][col] !== "O") continue;
-      let finalRow = row;
-      for (let tryRow = row + 1; tryRow < dish.length; ++tryRow) {
-        if (dish[tryRow][col] === ".") finalRow = tryRow;
-        else break;
-      }
-      if (finalRow !== row) {
-        dish[row][col] = ".";
-        dish[finalRow][col] = "O";
-      }
+      let move;
+      for (move = row + 1; move < dish.length; ++move) if (dish[move][col] !== ".") break;
+      if (move - 1 !== row) moveO(dish, row, col, move - 1, col);
     }
   }
 }
-function tiltWest(): void {
+function tiltWest(dish: Dish): void {
   for (let col = 0; col < dish[0].length; ++col) {
     for (let row = 0; row < dish.length; ++row) {
       if (dish[row][col] !== "O") continue;
-      let finalCol = col;
-      for (let tryCol = col - 1; tryCol >= 0; --tryCol) {
-        if (dish[row][tryCol] === ".") finalCol = tryCol;
-        else break;
-      }
-      if (finalCol !== col) {
-        dish[row][col] = ".";
-        dish[row][finalCol] = "O";
-      }
+      let move;
+      for (move = col - 1; move >= 0; --move) if (dish[row][move] !== ".") break;
+      if (move + 1 !== col) moveO(dish, row, col, row, move + 1);
     }
   }
 }
-function tiltEast(): void {
+function tiltEast(dish: Dish): void {
   for (let col = dish[0].length - 1; col >= 0; --col) {
     for (let row = 0; row < dish.length; ++row) {
       if (dish[row][col] !== "O") continue;
-      let finalCol = col;
-      for (let tryCol = col + 1; tryCol < dish[0].length; ++tryCol) {
-        if (dish[row][tryCol] === ".") finalCol = tryCol;
-        else break;
-      }
-      if (finalCol !== col) {
-        dish[row][col] = ".";
-        dish[row][finalCol] = "O";
-      }
+      let move;
+      for (move = col + 1; move < dish[0].length; ++move) if (dish[row][move] !== ".") break;
+      if (move - 1 !== col) moveO(dish, row, col, row, move - 1);
     }
   }
 }
 
-function spinCycle(): void {
-  tiltNorth();
-  tiltWest();
-  tiltSouth();
-  tiltEast();
+function spinCycle(dish: Dish): void {
+  tiltNorth(dish);
+  tiltWest(dish);
+  tiltSouth(dish);
+  tiltEast(dish);
 }
 
-function load(): number {
+function printDish(dish: Dish): void {
+  let print = "";
+  for (let row = 0; row < dish.length; ++row) {
+    for (let col = 0; col < dish[0].length; ++col) {
+      print += dish[row][col];
+    }
+    print += "\n";
+  }
+  console.log(print);
+}
+
+function countLoad(dish: Dish): number {
   let res = 0;
   for (let row = 0; row < dish.length; ++row) {
     for (let col = 0; col < dish[0].length; ++col) {
@@ -93,13 +87,19 @@ function load(): number {
   return res;
 }
 
-// Cycle detection: try to find a sequence of the same load()s after spinCycle(),
-// I arbitrarily chose this sequence to be 8 spin cycles long because that seemed long enough, and it worked!
+tiltNorth(dishOne);
+console.log("Part 1:", countLoad(dishOne));
+
+// CYCLE DETECTION: Try to find a sequence of the same n countLoad() values in a row (after each spin cycle),
+// I arbitrarily chose this sequence to be 8 spin cycles long because it seemed
+// unlikely that a sequence that long would result in a false positive, and it worked!
+// If the cycle length turns out to be shorter than 8 it should still work, it'll just
+// find a cycle length that is a multiple of the actual cycle length, which should still give the correct answer
 const cycleIndicators = new Map<string, number>();
 const indicatorBuilder: Array<string> = [];
 for (let i = 1; i < 1000; ++i) {
-  spinCycle();
-  const indicatorPart = load();
+  spinCycle(dishTwo);
+  const indicatorPart = countLoad(dishTwo);
   indicatorBuilder.push(indicatorPart.toString());
   if (indicatorBuilder.length > 8) indicatorBuilder.shift();
   const indicator = indicatorBuilder.join("_");
@@ -110,9 +110,9 @@ for (let i = 1; i < 1000; ++i) {
     const cycleLength = i - lastIndexSpotted;
     const stepsLeft = (1000000000 - i) % cycleLength;
     for (let j = 0; j < stepsLeft; ++j) {
-      spinCycle();
+      spinCycle(dishTwo);
     }
-    console.log("Part 2:", load());
+    console.log("Part 2:", countLoad(dishTwo));
     break;
   }
 }
